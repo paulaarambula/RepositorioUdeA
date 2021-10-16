08de7e169de45a5021c54a6fd4c2c8db0632044b
AlexisFAP <alexispatinoagudelo@gmail.com>
2021-10-16 15:46:42 -0500

    models, routers y controllers creados

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+//
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
+//
+const chalk = require('chalk')
 
 const app = express()
+
+const ventasRouter = require('./routers/venta-router')
+const usersRouter = require('./routers/user-router');
+const productosRouter = require('./routers/producto-router');
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser:true
@@ -10,7 +19,17 @@ mongoose.connect(process.env.MONGODB_URL, {
 .catch(err => console.log(err))
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+app.use('/venta', ventasRouter);
+app.use('/user', usersRouter);
+app.use('/producto', productosRouter);
+
+app.get('*', (request, response) => {
+    return response.send('Not found!');
+});
 
 app.set('port', process.env.PORT || 8000);
 
