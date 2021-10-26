5ee7bd0fe145f233c948e6b60d1fc3c7f7982e3d
paulaarambula <arambula.paula@gmail.com>
2021-10-26 15:30:14 -0500

    volver atras

--- models/product-model.js
+++ models/product-model.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
+    product: {
+        type: String,
+    },
     descripcion: {
         type: String,
         required: true,
